8d1da17707750b6580103957707502fa3d3fd54f
joseguillermojose01 <joseguillermojose01@gmail.com>
2026-06-07 05:39:12 +0000

    ui adelantada

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
+
 import { useEffect } from 'react';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { PaperProvider, MD3LightTheme } from 'react-native-paper';
 
 export default function RootLayout() {
   const [fontsLoaded, fontError] = useFonts({
@@ -19,8 +22,12 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <PaperProvider theme={MD3LightTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
